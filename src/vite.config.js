773e2e295a77a028a960fcebd55aa139e9e4cda1
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-04-08 22:05:13 +0700

    Initial commit

---
+++ src/vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    server: {
+        hmr: {
+            host: "localhost",
+        },
+        port: 5173,
+        host: true,
+    },
+    plugins: [
+        laravel({
+            input: 'resources/js/app.ts',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
