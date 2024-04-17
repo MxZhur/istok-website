f8e99a7cdd64cba15f37dd010fdc99ce526b9001
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-04-17 20:37:59 +0700

    Added Delete button to blog post items

--- src/resources/js/Pages/Admin/Blog/Partials/BlogPostItem.vue
+++ src/resources/js/Pages/Admin/Blog/Partials/BlogPostItem.vue
@@ -8,18 +8,41 @@
         <div>
             <TagPill v-for="tag in item.tags" :name="tag.name" />
         </div>
-        <div class="text-gray-600 text-sm text-right">
-            {{ item.created_at }}
+        <div class="flex flex-row justify-between items-center">
+            <div class="text-gray-600 text-sm">
+                {{ item.created_at }}
+                <template v-if="item.updated_at !== null && item.updated_at !== item.created_at">
+                    &nbsp; (ред. {{ item.updated_at }})
+                </template>
+            </div>
+            <div>
+                <DangerButton @click="deleteItem" title="Удалить">
+                    <TrashIcon />
+                </DangerButton>
+            </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import { Link } from '@inertiajs/vue3';
+import { Link, router } from '@inertiajs/vue3';
 import { BlogPostItemData } from '../Index.vue';
 import TagPill from '@/Components/TagPill.vue';
+import DangerButton from '@/Components/DangerButton.vue';
+import TrashIcon from '@/Components/Icons/TrashIcon.vue';
 
-defineProps<{
+const props = defineProps<{
     item: BlogPostItemData;
 }>();
+
+function deleteItem() {
+    if (!confirm('Вы уверены, что хотите удалить эту запись?')) {
+        return;
+    }
+
+    router.delete(route('admin.blog_posts.destroy', props.item.id), {
+        preserveScroll: true,
+        preserveState: true,
+    })
+}
 </script>
