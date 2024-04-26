2eab1dca6e606a5e8afe909047d9c2044d5b4e13
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-04-26 19:07:13 +0700

    Added "Other posts" to Home page

--- src/app/Http/Controllers/HomeController.php
+++ src/app/Http/Controllers/HomeController.php
@@ -10,10 +10,19 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $latestBlogPost = BlogPost::query()->latest()->first();
+        $latestBlogPost = BlogPost::query()
+            ->latest()
+            ->first();
+
+        $otherBlogPosts = BlogPost::query()
+            ->orderByDesc('created_at')
+            ->skip(1)
+            ->take(10)
+            ->get();
 
         return Inertia::render('Home', [
             'latestBlogPost' => $latestBlogPost,
+            'otherBlogPosts' => $otherBlogPosts,
         ]);
     }
 }
--- src/resources/js/Pages/Home.vue
+++ src/resources/js/Pages/Home.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
-import { BlogPostData } from '@/types';
+import { BlogPostData, BlogPostItemData } from '@/types';
 import SiteLayout from '@/Layouts/SiteLayout.vue';
 import BlogPostViewer from '@/Components/BlogPostViewer.vue';
 import MutedText from '@/Components/MutedText.vue';
+import BlogPostItem from './Blog/Partials/BlogPostItem.vue';
 
 defineProps<{
     latestBlogPost: BlogPostData | null;
+    otherBlogPosts: BlogPostItemData[];
 }>();
 
 </script>
@@ -18,7 +20,7 @@ defineProps<{
     <SiteLayout>
         <div v-if="latestBlogPost !== null">
             <BlogPostViewer :item="latestBlogPost" />
-            <div class="text-right">
+            <div class="text-right pb-4">
                 <Link :href="route('blog.show', latestBlogPost.id)">
                     На страницу статьи
                 </Link>
@@ -27,5 +29,14 @@ defineProps<{
         <MutedText v-else>
             На текущий момент новостей нет.
         </MutedText>
+
+        <div v-if="otherBlogPosts.length > 0">
+            <hr>
+            <div class="text-lg font-bold pt-4">
+                Другие статьи
+            </div>
+
+            <BlogPostItem v-for="item in otherBlogPosts" :item="item" :key="item.id" />
+        </div>
     </SiteLayout>
 </template>
