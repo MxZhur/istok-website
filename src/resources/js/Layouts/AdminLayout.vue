89cec86a7236cfcde776ef7890099134d89a444b
Maxim Zhuravlev <mxzhur@yandex.ru>
2024-04-28 20:00:02 +0700

    Integrated new logo

--- src/resources/js/Layouts/AdminLayout.vue
+++ src/resources/js/Layouts/AdminLayout.vue
@@ -45,7 +45,7 @@ const showingNavigationDropdown = ref(false);
                             <!-- Logo -->
                             <div class="shrink-0 flex items-center">
                                 <Link :href="route('admin.index')">
-                                <ApplicationLogo class="block h-9 w-auto fill-current text-gray-800" />
+                                <ApplicationLogo class="block h-12 w-auto fill-current text-gray-800" />
                                 </Link>
                             </div>
 
